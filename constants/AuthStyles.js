f2416acfdeb5bd29501f826f4362dc62d4733a24
Noah Sylwester <nanoabram@comcast.net>
2019-12-27 14:02:42 -0800

    adjust textinput color

--- constants/AuthStyles.js
+++ constants/AuthStyles.js
@@ -27,8 +27,8 @@ const styles = StyleSheet.create({
     textInput: {
       width: '50%',
       borderRadius: 7,
-      backgroundColor: 'rgba(255,255,255,0.7)',
-      color: 'whitesmoke',
+      backgroundColor: 'rgba(255,255,255,1)',
+      color: 'black',
       padding: 10,
       marginTop: 20,
       textAlign: 'center',
