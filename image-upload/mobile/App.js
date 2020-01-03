ae25b6335a188adfdbd5e66da25f74c53e912add
misosoup17 <misagaspar@icloud.com>
2020-01-02 21:27:54 -0800

    image

---
+++ image-upload/mobile/App.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { View, Text, Image, Button, Component, Fetch } from 'react-native'
+import ImagePicker from 'react-native-image-picker'
+
+export default class App extends React.Component {
+  state = {
+    photo: null,
+  }
+
+  handleChoosePhoto = () => {
+    const options = {
+      noData: true,
+    }
+    ImagePicker.launchImageLibrary(options, response => {
+      if (response.uri) {
+        this.setState({ photo: response })
+      }
+    })
+  }
+
+  render() {
+    const { photo } = this.state
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        {photo && (
+          <Image
+            source={{ uri: photo.uri }}
+            style={{ width: 300, height: 300 }}
+          />
+        )}
+        <Button title="Choose Photo" onPress={this.handleChoosePhoto} />
+      </View>
+    )
+  }
+}
+const createFormData = (photo, body) => {
+    const data = new FormData();
+  
+    data.append('photo', {
+      name: photo.fileName,
+      type: photo.type,
+      uri:
+        Platform.OS === 'android' ? photo.uri : photo.uri.replace('file://', ''),
+    });
+  
+    Object.keys(body).forEach(key => {
+      data.append(key, body[key]);
+    });
+  
+    return data;
+  };
+  
