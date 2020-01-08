604c8158aba5a9a5ac09b632de847cbe8e78be59
Noah Sylwester <nanoabram@comcast.net>
2020-01-08 10:03:28 -0800

    add stumps screen, stump screen to ui, modify navigator bar

---
+++ screens/StumpsScreen.js
@@ -0,0 +1,77 @@
+import * as WebBrowser from 'expo-web-browser';
+import React, { useState, useEffect } from 'react';
+import {
+  Image,
+  Platform,
+  View,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  Button
+} from 'react-native';
+import styles from '../constants/MainStyles';
+
+import { MonoText } from '../components/StyledText';
+
+
+export default function StumpsScreen(props) {
+
+
+    const [dataState, setDataState] = useState({
+        stumps: [],
+      })
+
+    useEffect(() => {
+      fetch('https://stump-around.herokuapp.com/stumps', {
+        method: 'GET',
+      })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        setDataState({
+          stumps: responseJson,
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    }, [])
+    
+
+    return (
+              <View style={styles.container}>
+                  <Text style={styles.hikesPageTitle}>
+                        Stumps
+                  </Text>
+                  <ScrollView
+                      keyboardShouldPersistTaps='never'
+                      style={styles.hikePageBody}
+                      contentContainerStyle={styles.hikesPageContentContainer}>
+                      {/* <Text style={styles.pageTitle}>
+                        Stumps
+                      </Text> */}
+                      {dataState.stumps.map((stump, i) => (
+                          <View style={styles.hikeContainer} key={i}>
+                              <Image source={{uri: stump.photo}} style={{ width: '100%', height: 200 }} />
+                              <View style={styles.hikeTag}>
+                                <TouchableOpacity onPress={() => props.navigation.navigate('Stump', { stump: stump })}>
+                                  <Text style={styles.title}>
+                                    {stump.name}
+                                  </Text>
+                                </TouchableOpacity>
+
+                                <Text style={styles.section}>
+                                    {stump.location}
+                                </Text>
+                              </View>
+                          </View>
+                      ))}
+                  </ScrollView>
+              </View>
+    )
+}
+
+StumpsScreen.navigationOptions = {
+  header: null,
+};
