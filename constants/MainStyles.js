7572f518052012792b03d3c0d3de0aa839864cc0
Noah Sylwester <nanoabram@comcast.net>
2020-01-19 18:46:54 -0800

    adjust more colors

--- constants/MainStyles.js
+++ constants/MainStyles.js
@@ -51,6 +51,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     },
     profileContainer: {
+      // backgroundColor: "#001a13",
       flex: 1,
       justifyContent: 'center',
       alignItems: 'center',
